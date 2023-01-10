7cfd87fa15c7dc35eac325e8460f9f0109489f2b
Aatif Syed <aatif@aatifsyed.uk>
2023-01-10 23:51:06 +0000

    feat: Positive<T>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,140 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Witness types for numbers.
+use std::{fmt, ops};
+
+/// A guarantee that `T > 0`.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, derive_more::AsRef)]
+#[repr(transparent)]
+pub struct Positive<T>(T);
+
+impl<T> Positive<T> {
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+    pub fn new_unchecked(value: T) -> Self {
+        Self(value)
+    }
+    pub fn map_unchecked(self, mut f: impl FnMut(T) -> T) -> Self {
+        Self(f(self.0))
+    }
+    pub fn mut_unchecked(&mut self) -> &mut T {
+        &mut self.0
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T> Positive<T>
+where
+    T: num::Zero + PartialOrd,
+{
+    pub fn new(value: T) -> Result<Self, NotPositive<T>> {
+        match value > T::zero() {
+            true => Ok(Self(value)),
+            false => Err(NotPositive(value)),
+        }
+    }
+    pub fn map(self, mut f: impl FnMut(T) -> T) -> Result<Self, NotPositive<T>> {
+        Self::new(f(self.0))
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub struct NotPositive<T>(pub T);
+
+impl<T: fmt::Display> fmt::Display for NotPositive<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("The value {} was not positive", self.0))
+    }
+}
+
+impl<T: fmt::Display + fmt::Debug> std::error::Error for NotPositive<T> {}
+
+////////////////////////////////
+// Positive<T> +  Positive<T> //
+// Positive<T> *  Positive<T> //
+// Positive<T> += Positive<T> //
+// Positive<T> *= Positive<T> //
+////////////////////////////////
+
+impl<T> ops::Add<Self> for Positive<T>
+where
+    T: ops::Add<T, Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        // - self is positive
+        // - rhs is positive
+        // - adding two positive integers will result in a positive integer
+        Self::new_unchecked(self.0 + rhs.0)
+    }
+}
+
+impl<T> ops::Mul<Self> for Positive<T>
+where
+    T: ops::Mul<T, Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // - self is positive
+        // - rhs is positive
+        // - multiplying two positive integers will result in a positive integer
+        Self::new_unchecked(self.0 * rhs.0)
+    }
+}
+
+impl<T> ops::AddAssign<Self> for Positive<T>
+where
+    T: ops::AddAssign<T>,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        // - self is positive
+        // - rhs is positive
+        // - adding two positive integers will result in a positive integer
+        self.mut_unchecked().add_assign(rhs.into_inner());
+    }
+}
+
+impl<T> ops::MulAssign<Self> for Positive<T>
+where
+    T: ops::MulAssign<T>,
+{
+    fn mul_assign(&mut self, rhs: Self) {
+        // - self is positive
+        // - rhs is positive
+        // - multiplying two positive integers will result in a positive integer
+        self.mut_unchecked().mul_assign(rhs.into_inner())
+    }
+}
+
+//////////////////////////////////////
+// Positive<UnsignedT> +  UnsignedT //
+// Positive<UnsignedT> += UnsignedT //
+//////////////////////////////////////
+
+impl<UnsignedT> ops::Add<UnsignedT> for Positive<UnsignedT>
+where
+    UnsignedT: num::Unsigned + ops::Add<UnsignedT, Output = UnsignedT>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: UnsignedT) -> Self::Output {
+        // - self is positive
+        // - rhs is positive or zero
+        // - adding two positive integers will result in a positive integer
+        // - adding zero to a positive integer will result in a positive integer
+        Self::new_unchecked(self.0 + rhs)
+    }
+}
+
+impl<T> ops::AddAssign<T> for Positive<T>
+where
+    T: num::Unsigned + ops::AddAssign<T>,
+{
+    fn add_assign(&mut self, rhs: T) {
+        // - self is positive
+        // - rhs is positive or zero
+        // - adding two positive integers will result in a positive integer
+        // - adding zero to a positive integer will result in a positive integer
+        self.mut_unchecked().add_assign(rhs)
     }
 }
